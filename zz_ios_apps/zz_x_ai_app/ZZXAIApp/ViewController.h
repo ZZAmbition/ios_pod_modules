e6b0f2180bdb111e852b9e93a11e748ac068270f
zhouzhi <zhi.zhou@kumai-inc.com>
2025-10-17 12:08:14 +0800

    新增壳应用

---
+++ zz_ios_apps/zz_x_ai_app/ZZXAIApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ZZXAIApp
+//
+//  Created by zz on 2025/10/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ zz_ios_apps/zz_x_ai_app/ZZXAIApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZZXAIApp
+//
+//  Created by zz on 2025/10/17.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
